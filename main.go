aa12091bf2dd38cdabc93d52b6c22e77f143f521
ChengyuZhu6 <chengyu.zhu@intel.com>
2024-05-20 16:30:49 +0800

    test
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

--- main.go
+++ main.go
@@ -199,9 +199,10 @@ func main() {
 		parameters := map[string]map[string]string{}
 		for proto, pkg := range c.Packages {
 			for _, gen := range c.Generators {
-			parameters[gen] = mergeMap(parameters[gen], map[string]string{
-				fmt.Sprintf("M%s", proto): pkg,
-			})
+				parameters[gen] = mergeMap(parameters[gen], map[string]string{
+					fmt.Sprintf("M%s", proto): pkg,
+				})
+			}
 		}
 
 		for k, v := range c.Parameters {
